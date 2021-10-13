8d4466c2b09d16fcb94e7e0be9630035c4171286
pk <>
2021-10-13 16:30:56 +0530

    2nd commit

--- springLab2/src/main/java/CollectionTypeInjection/App.java
+++ springLab2/src/main/java/CollectionTypeInjection/App.java
@@ -1,5 +1,8 @@
 package CollectionTypeInjection;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
+        Emp emp1=(Emp)context.getBean("emp1");
+        System.out.println(emp1.getName());
+        System.out.println(emp1.getPhones());
+        System.out.println(emp1.getAddress());
+        System.out.println(emp1.getCourses());
+        
     }
 }
